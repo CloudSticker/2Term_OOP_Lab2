4d5e86d1122be9cf2fad730992b4d81b27eb5d38
Daniil <dan9154634874@gmail.com>
2021-03-06 23:32:44 +0300

    Upgrade 3rd game KDD

--- TaskD_3.xaml.cs
+++ TaskD_3.xaml.cs
@@ -19,12 +19,22 @@ namespace WpfApp1
     /// </summary>
     public partial class Window7 : Window
     {
+        Random rnd = new Random();
         Graph graph = new Graph();
         List<Line> lineList = new List<Line>();
+        int[,] arr = new int[10, 10];
+        int[,] mass = new int[10, 10];
 
         public Window7()
         {
             InitializeComponent();
+            
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    arr[i, j] = rnd.Next(0, 2);
+                }
+            
         }
 
 
@@ -35,18 +45,52 @@ namespace WpfApp1
             for (int i = 0; i < lineList.Count; i++)
                 Canvas1.Children.Add(lineList[i]);
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Random rnd = new Random();
             ClearCanvas();
-            for(int i = 0; i < 10; i++)
+            
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    AddNum(i * 50 + 5, j * 50 + 15, Convert.ToString(arr[i, j]));
+            Generation();
+            arr = mass;
+
+        }
+
+        void Generation()
+        {
+            for (int i = 0; i < 10; i++)
                 for (int j = 0; j < 10; j++)
                 {
-                    AddNum(j * 50 + 5, i * 50 + 15, Convert.ToString(rnd.Next(0, 9)));  
+                    int k = neighbors(i, j);
+                    if ((k < 2)||(k > 3))
+                    {
+                        mass[i, j] = 0;
+                    }else if (k == 3)
+                    {
+                        mass[i, j] = 1;
+                    }
+                    else
+                    {
+                        mass[i, j] = arr[i, j];
+                    }
                 }
-                   
-
+        }
+        //(((j1 >= 1) && (j1 <= 9))&&((i1 >= 1) && (i1 <= 9)) && ((i != i1)&&(j != j1)))
+        public int neighbors(int i1, int j1)
+        {
+            int k = 0;
+            for (int i = (i1 - 1); i <= (i1 + 1); i++)
+                for (int j = (j1 - 1); j <= (j1 + 1); j++)
+                {
+                    if (((i != -1)&&( j != -1)) && ((i != i1) || (j != j1)))
+                    {
+                        if (((j >= 0) && (j <= 9)) && ((i >= 0) && (i <= 9)))
+                            if (arr[i, j] == 1)
+                                k++;
+                    }
+                }
+            return k;
         }
 
         void AddNum(int X, int Y, string num)
